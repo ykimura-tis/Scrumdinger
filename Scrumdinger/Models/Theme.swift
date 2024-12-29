4ea40d3523f354be5c0625a562f5e88e75f94e3c
Yukitoshi Kimura <kimura_yukitosi@r5.ucom.ne.jp>
2024-12-29 17:29:16 +0900

    Creating a navigation hierarchy

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -31,8 +31,12 @@ enum Theme: String {
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
-
+    
     var mainColor: Color {
         Color(rawValue)
+    }
+    
+    var name: String {
+        rawValue.capitalized
     }
 }

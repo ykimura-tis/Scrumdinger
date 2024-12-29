5fa32fd434a4d54fd9f37e18e6ddff9cdbdf74fa
Yukitoshi Kimura <kimura_yukitosi@r5.ucom.ne.jp>
2024-12-29 11:10:33 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 木村幸敏 on 2024/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

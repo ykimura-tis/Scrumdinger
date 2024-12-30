116a9f4805b64dd143f432bff2e025d789b6857c
Yukitoshi Kimura <kimura_yukitosi@r5.ucom.ne.jp>
2024-12-30 13:14:29 +0900

    Managing state and life cycle

---
+++ Scrumdinger/Views/MeetingView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 木村幸敏 on 2024/12/29.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @StateObject var scrumTimer = ScrumTimer()
+    
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+            }
+        }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear {
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+            scrumTimer.startScrum()
+        }
+        .onDisappear {
+            scrumTimer.stopScrum()
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
+}

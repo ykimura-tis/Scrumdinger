f623f3ce87a4087ce69255753e2acaacba3b108a
Yukitoshi Kimura <kimura_yukitosi@r5.ucom.ne.jp>
2025-01-03 12:43:46 +0900

    Recording Audio

---
+++ Scrumdinger/Views/HistoryView.swift
@@ -0,0 +1,51 @@
+//
+//  HistoryView.swift
+//  Scrumdinger
+//
+//  Created by 木村幸敏 on 2025/01/03.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    let history: History
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading) {
+                Divider()
+                    .padding(.bottom)
+                Text("Attendees")
+                    .font(.headline)
+                Text(history.attendeeString)
+                if let transcript = history.transcript {
+                    Text("Transcript")
+                        .font(.headline)
+                        .padding(.top)
+                    Text(transcript)
+                }
+            }
+        }
+        .navigationTitle(Text(history.date, style: .date))
+        .padding()
+    }
+}
+
+extension History {
+    var attendeeString: String {
+        ListFormatter.localizedString(byJoining: attendees.map { $0.name })
+    }
+}
+
+#Preview {
+    var history: History {
+        History(attendees: [
+            DailyScrum.Attendee(name: "Jon"),
+            DailyScrum.Attendee(name: "Darla"),
+            DailyScrum.Attendee(name: "Luis")
+        ],
+                transcript: "Darla, would you like to start today? Sure, yesterday I reviewed Luis' PR and met with the design team to finalize the UI...")
+    }
+
+    HistoryView(history: history)
+}
